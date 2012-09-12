5c2348d8cbbfe5d79605853870b2fa4804ff3708
Damian Senn <damian.senn@gmail.com>
2012-09-12 18:59:35 +0200

    add name and done field to tasks

--- schemas/task.js
+++ schemas/task.js
@@ -2,8 +2,10 @@ var Schema = require('mongoose').Schema
 
 var Task = module.exports = new Schema
 
-Task.add({ 'duration': Number
-         , 'from':     Date,
+Task.add({ 'name':     String
+         , 'duration': Number
+         , 'from':     Date
          , 'to':       Date
          , 'tasks':    [ Task ]
+         , 'done':     Boolean
          })

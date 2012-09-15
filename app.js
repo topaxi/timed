68852a6fe140a6f6a0f519d88ebb7d475fb54641
Damian Senn <damian.senn@gmail.com>
2012-09-15 17:40:14 +0200

    minor refactoring

--- app.js
+++ app.js
@@ -33,9 +33,7 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user)
-  });
+  User.findById(id, done);
 });
 
 app.configure(function() {

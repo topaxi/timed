e8ac9be4224aca2c81b9d7199786027002428a08
Damian Senn <damian.senn@adfinis-sygroup.ch>
2014-10-09 16:19:52 +0200

    Remove console.log() from user route.

--- frontend/app/routes/user.js
+++ frontend/app/routes/user.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var users = this.store.find('user')
-    users.then(function(users) { console.log(users) })
-    return users
+    return this.store.find('user')
   }
-});
+})

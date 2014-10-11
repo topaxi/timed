9fe947dfe2382a9cf8b82e26793aa0cd70b556d5
Damian Senn <damian.senn@gmail.com>
2014-10-11 15:09:06 +0200

    Remove semicolons from router.js

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -3,14 +3,16 @@ import config from './config/environment';
 
 var Router = Ember.Router.extend({
   location: config.locationType
-});
+})
 
 Router.map(function() {
-  this.route('login');
+  this.route('login')
+
   this.resource('user', function() {
     this.route('edit', { 'path': '/:user_id' })
-  });
-  this.route('about');
-});
+  })
 
-export default Router;
+  this.route('about')
+})
+
+export default Router

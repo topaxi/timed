7322371d68404641ef190945a2b05eebf57a332e
Damian Senn <damian.senn@gmail.com>
2014-10-11 15:06:30 +0200

    Fix success message after saving users.

--- frontend/app/controllers/user/edit.js
+++ frontend/app/controllers/user/edit.js
@@ -4,10 +4,10 @@ import Notify from 'ember-notify';
 export default Ember.ObjectController.extend({
   actions: {
     submit: function() {
-      this.model.save(function(err) {
-        if (err) return Notify.error(err)
-
-        Notify.ok('User successfully saved!')
+      this.model.save().then(function() {
+        Notify.success('User successfully saved!')
+      }, function(err) {
+        return Notify.error(err || 'Error while trying to save user!')
       })
     }
   }

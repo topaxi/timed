f3c695d2ba88be11913139e84c55128668776e95
Damian Senn <damian.senn@adfinis-sygroup.ch>
2014-10-16 11:34:10 +0200

    Use singlequotes.

--- backend/app.js
+++ backend/app.js
@@ -99,5 +99,5 @@ app.use(function(err, req, res, next) {
 })
 
 http.createServer(app).listen(app.get('port'), function() {
-  console.log("Express server listening on port " + app.get('port'))
+  console.log('Express server listening on port ' + app.get('port'))
 })

972f2a7c91b109c6061f1e489c9d65f7c0c5ad45
Damian Senn <damian.senn@adfinis-sygroup.ch>
2014-10-20 12:53:20 +0200

    Enable ssl for vagrant nginx config.

--- tools/vagrant/provision.sh
+++ tools/vagrant/provision.sh
@@ -25,12 +25,25 @@ function install_nginx() {
   # Fix weird caching behaviour - see http://stackoverflow.com/q/6236078/35189
   sed -i -e 's/sendfile\s\+on/sendfile off/' /etc/nginx/nginx.conf
 
-  ln -sf /vagrant/tools/vagrant/nginx/timed.conf /etc/nginx/conf.d/timed.conf
+  ln -sf /vagrant/tools/vagrant/nginx/vagrant-timed.conf /etc/nginx/conf.d/001-timed.conf
 
   rm -f /etc/nginx/conf.d/default.conf
   rm -f /etc/nginx/sites-enabled/default.conf
 
+  setup_nginx_ssl
+
   service nginx restart
+}
+
+function setup_nginx_ssl() {
+  mkdir -p /etc/nginx/ssl
+
+  cd /etc/nginx/ssl
+
+  openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out server.key
+  chmod 600 server.key
+  openssl req -new -key server.key -out server.csr -subj "/C=CH/ST=Bern/L=Bern/O=timed/CN=timed.vm"
+  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 }
 
 function install_node() {

7be2010671721bddac7aa45bc6f9c8feff81d618
Damian Senn <damian.senn@gmail.com>
2014-10-22 23:44:59 +0200

    Remove TODO for fixed issue.

--- frontend/app/models/project.js
+++ frontend/app/models/project.js
@@ -2,9 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   'name':     DS.attr('string')
-  // TODO: Async customer sometimes messes up the edit form after some transitions
-  //       probably because we're comparing with a promise instead of the
-  //       actual object / id
 , 'customer': DS.belongsTo('customer', { 'async': true })
 , 'tasks':    DS.hasMany('task', { 'async': true })
 , 'from':     DS.attr('moment')

04d45bbb6e01399ca50a1e47cb579a994a054654
Damian Senn <damian.senn@gmail.com>
2014-11-08 20:03:47 +0100

    Attendance.user should be async.

--- frontend/app/models/attendance.js
+++ frontend/app/models/attendance.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import moment from 'moment';
 
 export default DS.Model.extend({
-  'user':       DS.belongsTo('user')
+  'user':       DS.belongsTo('user', { 'async': true })
 , 'activities': DS.hasMany('activity')
 , 'from':       DS.attr('moment')
 , 'to':         DS.attr('moment')

7da61f32ebeb65af1fc56da7e8541f94ba84f8ab
Damian Senn <damian.senn@adfinis-sygroup.ch>
2014-11-11 10:45:25 +0100

    Quote properties and add some blank lines for readability.

--- frontend/app/controllers/modal/activity.js
+++ frontend/app/controllers/modal/activity.js
@@ -2,36 +2,42 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.ObjectController.extend({
-  dateFormat: 'YYYY-MM-DD hh:mm'
-, init: function() {
+  'dateFormat': 'YYYY-MM-DD hh:mm'
+
+, 'init': function() {
     this.set('tasks', this.store.find('task'))
   }
-, sortedTasks: function() {
+
+, 'sortedTasks': function() {
     return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
-      content:        this.get('tasks')
-    , sortProperties: [ 'project.name' ]
-    , sortAscending:  false
+      'content':        this.get('tasks')
+    , 'sortProperties': [ 'project.name' ]
+    , 'sortAscending':  false
     })
   }.property('tasks')
-, createActivityForAttendance: function() {
+
+, 'createActivityForAttendance': function() {
     if (this.get('model.activities')) {
       this.set('model', this.store.createRecord('activity', {
         'attendance': this.get('model')
       }))
     }
   }.observes('model')
-, from: function() {
+
+, 'from': function() {
     var from = this.get('model.from')
 
     return from && from.format(this.dateFormat)
   }.property('model.from')
-, to: function() {
+
+, 'to': function() {
     var to = this.get('model.to')
 
     return to && to.format(this.dateFormat)
   }.property('model.to')
-, actions: {
-    save: function() {
+
+, 'actions': {
+    'save': function() {
       var activity = this.get('model')
 
       activity.set('from', moment(this.get('from'), this.dateFormat))
@@ -39,7 +45,8 @@ export default Ember.ObjectController.extend({
 
       activity.get('attendance').save()
     }
-  , closeModal: function() {
+
+  , 'closeModal': function() {
       this.get('model').rollback()
 
       return true

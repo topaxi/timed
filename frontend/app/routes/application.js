57e0c1aee75880bdcd6ca114af5e8ee8869cfe81
Damian Senn <damian.senn@adfinis-sygroup.ch>
2014-11-11 15:07:49 +0100

    Simplify openModal action.

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -4,11 +4,11 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 export default Ember.Route.extend(ApplicationRouteMixin, {
   'actions': {
     'openModal': function(name, model) {
-      this.controllerFor(`modal.${name}`).set('model', model)
-
       return this.render(`modal/${name}`, {
-        'into':   'application'
-      , 'outlet': 'modal'
+        'into':       'application'
+      , 'outlet':     'modal'
+      , 'controller': `modal.${name}`
+      , 'model':      model
       })
     }
   , 'closeModal': function() {

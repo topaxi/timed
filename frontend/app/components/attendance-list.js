68800a126a593bc5724fd511eb7500317a584d9a
Damian Senn <damian.senn@gmail.com>
2015-01-08 20:08:08 +0100

    Fix missing super call in component init method.

--- frontend/app/components/attendance-list.js
+++ frontend/app/components/attendance-list.js
@@ -3,11 +3,20 @@ import moment from 'moment';
 
 export default Ember.Component.extend({
   currentDay: null
+
 , init: function() {
+    this._super()
     this.set('currentDay', moment().startOf('day'))
   }
+
 , attendances: function() {
     return this.get('user').getAttendancesByDay(this.get('currentDay'))
                            .sort((a, b) => b.get('from').diff(a.get('from')))
   }.property('currentDay', 'user.attendances.@each')
+
+, actions: {
+    openModal(...args) {
+      this.sendAction('openModal', ...args)
+    }
+  }
 })

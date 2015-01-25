7f516af16f9f8d50ce696de7a60491a9c1a44488
Damian Senn <damian.senn@gmail.com>
2015-01-25 22:24:46 +0100

    Redirect from login route if already authenticated.

--- frontend/app/routes/login.js
+++ frontend/app/routes/login.js
@@ -1,6 +1,7 @@
-import Ember from 'ember';
+import Ember                     from 'ember'
+import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin'
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   afterModel() {
     if (this.session.isAuthenticated) {
       this.replaceWith('/')

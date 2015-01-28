8ee021b422de1ac8c114934e5ca4aa9887791d84
Damian Senn <damian.senn@gmail.com>
2015-01-28 21:58:51 +0100

    Denodify bin/user.js

--- backend/bin/user.js
+++ backend/bin/user.js
@@ -2,28 +2,32 @@
 
 require('6to5/register')
 
-var config   = require('../config.json')
-  , mongoose = require('mongoose')
-  , read     = require('read')
-  , User     = require('../models/user')
-  , user     = new User
+var config      = require('../config.json')
+var RSVP        = require('rsvp')
+var mongoose    = require('mongoose')
+var read        = RSVP.denodeify(require('read'))
+var User        = require('../models/user')
+var user        = new User
+var save        = RSVP.denodeify(user.save.bind(user))
+var setPassword = RSVP.denodeify(user.setPassword.bind(user))
 
 mongoose.connect(config.mongodb)
 
-read({ 'prompt': 'Username: ' }, function(err, username) {
-  user.name = username
+read({ 'prompt': 'Username: ' })
+  .then(function(username) {
+    user.name = username
 
-  read({ 'prompt': 'Password: ', 'silent': true }, function(err, password) {
-    user.setPassword(password, function(err) {
-      if (err) throw err
-
-      user.save(function(err) {
-        if (err) throw err
-
-        console.log('User', username, 'created')
-
-        process.exit()
-      })
-    })
+    return read({ 'prompt': 'Password: ', 'silent': true }).then(setPassword)
   })
-})
+  .then(function() {
+    return save()
+  })
+  .then(function() {
+    console.log('User', user.name, 'created')
+  })
+  .catch(function(err) {
+    console.error('Error %s: %s', err.code, err.message)
+  })
+  .finally(function() {
+    process.exit()
+  })

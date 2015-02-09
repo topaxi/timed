71b306436db4ace89dc7e221f230e24579c43bdf
Damian Senn <damian.senn@gmail.com>
2015-02-09 21:26:48 +0100

    Transition after animation for smoother mobile experience.

--- frontend/app/views/application.js
+++ frontend/app/views/application.js
@@ -2,8 +2,24 @@ import Ember from 'ember'
 
 export default Ember.View.extend({
   collapseMenuItems: function() {
-    this.$().find('.navbar-collapse').on('click', '[href]', () =>
-      this.$().find('.navbar-toggle:visible').click()
-    )
+    let $collapse = this.$('.navbar-collapse')
+    let $toggle   = this.$('.navbar-toggle')
+
+    $collapse.on('click', '[href]', e => {
+      if (!$toggle.is(':visible')) {
+        return
+      }
+
+      e.preventDefault()
+      e.stopImmediatePropagation()
+
+      $toggle.click()
+
+      let href       = e.currentTarget.getAttribute('href')
+      let controller = this.get('controller')
+      let transition = () => controller.transitionToRoute(href)
+
+      $collapse.on('hidden.bs.collapse', transition)
+    })
   }.on('didInsertElement')
 })

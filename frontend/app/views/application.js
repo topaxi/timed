28b048f549c405d32dcd4a55ca354214c56afa8d
Damian Senn <damian.senn@gmail.com>
2015-02-09 11:13:50 +0100

    Fix hiding menu on non-mobile screens.

--- frontend/app/views/application.js
+++ frontend/app/views/application.js
@@ -2,10 +2,8 @@ import Ember from 'ember'
 
 export default Ember.View.extend({
   collapseMenuItems: function() {
-    let $navbarCollapse = this.$().find('.navbar-collapse')
-
-    $navbarCollapse.on('click', '[href]', () =>
-      $navbarCollapse.collapse('hide')
+    this.$().find('.navbar-collapse').on('click', '[href]', () =>
+      this.$().find('.navbar-toggle:visible').click()
     )
   }.on('didInsertElement')
 })

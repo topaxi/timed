884a5684bb3ab2ba4f3d0e22896cb2f771d956c1
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-02-10 12:10:23 +0100

    Fetch all issues for a redmine project.

--- frontend/app/utils/autocomplete/redmine.js
+++ frontend/app/utils/autocomplete/redmine.js
@@ -1,28 +1,47 @@
 import Ember from 'ember'
 
+const LIMIT = 100 // Maximum limit is 100
+
 export default Ember.Object.extend({
 
   issues: null
 
-  // Redmine API does not allow to search for issues...
-  // See: http://www.redmine.org/issues/6277
 , searchIssues(term = '') {
     if (this.get('issues')) {
       return this.get('issues')
     }
 
+    this.set('issues', this._fetch().then(issues => issues.map(i => i.subject)))
+  }
+
+  // Redmine API does not allow to search for issues.
+  // For now, we fetch all issues for a project...
+  // See: http://www.redmine.org/issues/6277
+, _fetch(offset = 0, issues = []) {
     let data = this.project.get('tracker.data')
 
-    this.set('issues', Ember.$.ajax({
+    return Ember.$.ajax({
       url: `${data.url}/issues.json`
     , data: {
         'project_id': data.projectId
-      , 'limit': 100 // Maximum limit is 100
+      , 'limit':      LIMIT
+      , offset
       }
     , headers: {
         'X-Redmine-API-Key': data.apikey
       }
     })
-    .then(res => res.issues.map(i => i.subject)))
+    .then(res => {
+      issues.push.apply(issues, res.issues)
+
+      return res
+    })
+    .then(res => {
+      if (res.total_count > res.offset + res.limit) {
+        return this._fetch(res.offset + res.limit, issues)
+      }
+
+      return issues
+    })
   }
 })

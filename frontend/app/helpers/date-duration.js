29e4e5496d72ec144dbdad703148da0cdaefdd72
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-02-13 15:40:07 +0100

    Reduce indentation of 3 spaces to 2 spaces...

--- frontend/app/helpers/date-duration.js
+++ frontend/app/helpers/date-duration.js
@@ -2,23 +2,23 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Handlebars.makeBoundHelper(function(date1, date2, format) {
-   if (!date1) {
-     return ''
-   }
+  if (!date1) {
+    return ''
+  }
 
-   if (!date2 || !date2.isValid()) {
-     var html = `<span data-from-now="true" data-from="${date1}">${date1.fromNow(true)}</span>`
+  if (!date2 || !date2.isValid()) {
+    var html = `<span data-from-now="true" data-from="${date1}">${date1.fromNow(true)}</span>`
 
-     return new Ember.Handlebars.SafeString(html)
-   }
+    return new Ember.Handlebars.SafeString(html)
+  }
 
-   if (typeof format !== 'string') {
-     format = 'h[h] mm[m]'
-   }
+  if (typeof format !== 'string') {
+    format = 'h[h] mm[m]'
+  }
 
-   var diff      = Math.abs(moment(date1).diff(moment(date2)))
-   var duration  = moment.duration(diff)
-   var formatted = duration.format(format)
+  var diff      = Math.abs(moment(date1).diff(moment(date2)))
+  var duration  = moment.duration(diff)
+  var formatted = duration.format(format)
 
-   return formatted
+  return formatted
 })

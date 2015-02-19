4bb20804efce2e2942c0eeaf05a9e3521546a709
Damian Senn <damian.senn@gmail.com>
2015-02-19 21:47:09 +0100

    Fix various controller unit tests.

--- frontend/tests/unit/controllers/modal/activity-test.js
+++ frontend/tests/unit/controllers/modal/activity-test.js
@@ -1,12 +1,24 @@
-import { moduleFor, test } from 'ember-qunit';
+import Ember               from 'ember'
+import startApp            from '../../../helpers/start-app'
+import { moduleFor, test } from 'ember-qunit'
+
+let App
+let store
 
 moduleFor('controller:modal/activity', 'ModalActivityController', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
-});
+  setup() {
+    App   = startApp()
+    store = App.__container__.lookup('store:main')
+  }
+, teardown() {
+    Ember.run(() => App.destroy())
+  }
+})
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
-  var controller = this.subject();
-  assert.ok(controller);
-});
+  var controller = this.subject({ store })
+  assert.ok(controller)
+})

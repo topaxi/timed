97971bdccc80f6bf15cbc0f87cb2e3075f739667
Damian Senn <damian.senn@gmail.com>
2015-02-19 21:32:52 +0100

    Fix attendance-list unit test.

--- frontend/tests/unit/components/attendance-list-test.js
+++ frontend/tests/unit/components/attendance-list-test.js
@@ -1,17 +1,31 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import Ember                        from 'ember'
+import startApp                     from '../../../helpers/start-app'
+import { moduleForComponent, test } from 'ember-qunit'
+
+let App
+let store
 
 moduleForComponent('attendance-list', 'AttendanceListComponent', {
-  needs: [ 'component:date-picker' ]
-});
+  needs: [ 'component:date-picker', 'model:user' ]
+, setup() {
+    App   = startApp()
+    store = App.__container__.lookup('store:main')
+  }
+, teardown() {
+    Ember.run(() => App.destroy())
+  }
+})
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  assert.expect(2)
+
+  let user = store.createRecord('user')
 
   // creates the component instance
-  var component = this.subject();
-  assert.equal(component._state, 'preRender');
+  var component = this.subject({ user })
+  assert.equal(component._state, 'preRender')
 
   // appends the component to the page
-  this.render();
-  assert.equal(component._state, 'inDOM');
-});
+  this.render()
+  assert.equal(component._state, 'inDOM')
+})

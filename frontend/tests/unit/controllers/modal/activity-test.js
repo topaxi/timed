c8a4740a291405be5bc53f3993c491afdfbda13d
Damian Senn <damian.senn@gmail.com>
2015-02-19 22:07:40 +0100

    Disable real store in activity-test.
    There is something really weird going on which leakes global variables.

--- frontend/tests/unit/controllers/modal/activity-test.js
+++ frontend/tests/unit/controllers/modal/activity-test.js
@@ -3,17 +3,17 @@ import startApp            from '../../../helpers/start-app'
 import { moduleFor, test } from 'ember-qunit'
 
 let App
-let store
+let store = { find: () => void 0 }
 
 moduleFor('controller:modal/activity', 'ModalActivityController', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
   setup() {
-    App   = startApp()
-    store = App.__container__.lookup('store:main')
+    // App   = startApp()
+    // store = App.__container__.lookup('store:main')
   }
 , teardown() {
-    Ember.run(() => App.destroy())
+    // Ember.run(() => App.destroy())
   }
 })
 

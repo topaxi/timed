d1b1bea298649cf46fae32c0f14d47d52ab4ecda
Damian Senn <damian.senn@gmail.com>
2015-02-28 22:00:17 +0100

    Install ember-cli-blanket for code coverage reports.

---
+++ frontend/tests/blanket-options.js
@@ -0,0 +1,9 @@
+/* globals blanket */
+
+blanket.options({
+   modulePrefix: "timed",
+   filter: "//.*timed/.*/",
+   antifilter: "//.*(tests|template).*/",
+   loaderExclusions: [],
+   enableCoverage: true
+});

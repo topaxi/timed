eb63a302ffe723f6f6909e74779c6599ea79df46
Damian Senn <damian.senn@gmail.com>
2015-03-01 19:29:53 +0100

    Unit tests for FollowProjectComponent

--- frontend/app/components/follow-project.js
+++ frontend/app/components/follow-project.js
@@ -1,11 +1,12 @@
-import Ember from 'ember';
-import Notify from 'ember-notify';
+import Ember from 'ember'
 
 export default Ember.Component.extend({
   tagName: 'span'
+
 , title: function() {
     return `${this.get('isFollowing') ? 'Unfollow' : 'Follow'} ${this.get('project.name')}`
   }.property('project.name', 'isFollowing')
+
 , fixTooltip: function() {
     let tooltip = this.$('.tip')
 
@@ -16,6 +17,7 @@ export default Ember.Component.extend({
       tooltip.tooltip('show')
     }
   }.observes('title')
+
 , isFollowing: function() {
     let projects = this.get('session.user.projects')
 
@@ -25,26 +27,15 @@ export default Ember.Component.extend({
 
     return projects.contains(this.get('project'))
   }.property('session.user.projects.@each')
+
 , actions: {
     follow: function() {
       let user     = this.session.get('user')
-        , projects = user.get('projects')
+      let projects = user.get('projects')
+      let action   = this.get('isFollowing') ? 'removeObject' : 'addObject'
 
-      if (!projects) {
-        Notify.error('Something went wrong!')
-      }
-
-      let action = this.get('isFollowing') ? 'removeObject' : 'addObject'
-
-      // TODO: For some reason, if we remove either projects[action](project)
-      //       call, the project won't get stored in the user.
       projects[action](this.get('project'))
-      user.then(user =>
-        user.get('projects').then(projects => {
-          projects[action](this.get('project'))
-          user.save()
-        })
-      )
+      user.save()
     }
   }
 })
--- frontend/tests/unit/components/follow-project-test.js
+++ frontend/tests/unit/components/follow-project-test.js
@@ -6,7 +6,7 @@ let App
 let store
 
 moduleForComponent('follow-project', 'FollowProjectComponent', {
-  needs: [ 'model:project' ]
+  needs: [ 'model:project', 'model:user' ]
 , setup() {
     App   = startApp()
     store = App.__container__.lookup('store:main')
@@ -23,12 +23,85 @@ test('it renders', function(assert) {
     let project = store.createRecord('project')
     let session = { user: Ember.Object.create() }
 
-    // creates the component instance
-    var component = this.subject({ project, session })
+    let component = this.subject({ project, session })
     assert.equal(component._state, 'preRender')
 
-    // appends the component to the page
     this.render()
     assert.equal(component._state, 'inDOM')
+  })
+})
+
+test('it updates the tooltip', function(assert) {
+  assert.expect(2)
+
+  Ember.run(() => {
+    let project = store.createRecord('project', { name: 'Proejct 1' })
+    let session = { user: Ember.Object.create() }
+
+    let component = this.subject({ project, session })
+
+    this.render()
+
+    let $tip = component.$('.tip')
+    assert.equal($tip.prop('title'), 'Follow Proejct 1')
+
+    project.set('name', 'Project 1')
+
+    $tip.trigger('mouseover')
+
+    assert.equal($tip.parent().find('.tooltip').text(), 'Follow Project 1')
+  })
+})
+
+test('it sets the isFollowing attribute', function(assert) {
+  assert.expect(2)
+
+  Ember.run(() => {
+    let project = store.createRecord('project', { name: 'Project 1' })
+    let session = { user: Ember.Object.create({ projects: [ project ] }) }
+
+    let component = this.subject({ project, session })
+
+    this.render()
+
+    assert.ok(component.get('isFollowing'))
+
+    let $tip = component.$('.tip')
+    assert.equal($tip.prop('title'), 'Unfollow Project 1')
+  })
+})
+
+test('it updated the users projects', function(assert) {
+  assert.expect(4)
+
+  let user
+  let component
+
+  Ember.run(() => {
+    let project = store.createRecord('project', { name: 'Project 1' })
+    user    = store.createRecord('user')
+    let session = Ember.Object.create({ user })
+
+    component = this.subject({ project, session })
+
+    this.render()
+  })
+
+  andThen(() => {
+    click(component.$('button'))
+  })
+
+  andThen(() => {
+    assert.equal(user.get('projects.length'), 1)
+    assert.equal(component.get('isFollowing'), true)
+  })
+
+  andThen(() => {
+    click(component.$('button'))
+  })
+
+  andThen(() => {
+    assert.equal(user.get('projects.length'), 0)
+    assert.equal(component.get('isFollowing'), false)
   })
 })

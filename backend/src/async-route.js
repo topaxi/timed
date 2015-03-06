53d38347452f6af052bdf981451383e0bfc0ae0f
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-03-06 15:41:48 +0100

    No need for async error routes

--- backend/src/async-route.js
+++ backend/src/async-route.js
@@ -1,12 +1,6 @@
 import co from 'co'
 
 export function async(fn) {
-  if (fn.length === 4) {
-    return function(err, req, res, next) {
-      return co(fn.call(this, err, req, res, next)).catch(next)
-    }
-  }
-
   return function(...args) {
     return co(fn.apply(this, args)).catch(args[args.length - 1])
   }

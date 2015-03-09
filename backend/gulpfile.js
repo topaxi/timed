5e67ec0da471266869bcff94ed43c1d4cc2256d6
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-03-09 12:26:31 +0100

    Use gulp for testruns to enforce coverage

---
+++ backend/gulpfile.js
@@ -0,0 +1,59 @@
+var gulp             = require('gulp')
+var istanbul         = require('gulp-istanbul')
+var mocha            = require('gulp-mocha')
+var coverageEnforcer = require('gulp-istanbul-enforcer');
+var isparta          = require('isparta')
+
+var src = [
+  'src/**/*.js'
+, 'routes/**/*.js'
+, 'middleware/*.js'
+, 'models/*.js'
+]
+
+var testFiles = [ 'test/**/*-test.js' ]
+var reports   = [ 'lcov', 'json', 'html', 'text', 'text-summary' ]
+
+gulp.task('test', function(done) {
+  gulp.src(src)
+    .pipe(istanbul({
+      instrumenter:    isparta.Instrumenter
+    //, includeUntested: true
+    }))
+    .pipe(istanbul.hookRequire())
+    .pipe(gulp.dest('test-tmp/'))
+    .on('finish', function() {
+      gulp.src(testFiles)
+        .pipe(mocha({ reporter: 'spec' }))
+        .pipe(istanbul.writeReports(reports))
+        .pipe(coverageEnforcer({
+          thresholds: {
+            statements: 99
+          , branches: 96
+          , lines: 100
+          , functions: 100
+          }
+        , coverageDirectory: 'coverage'
+        , rootDirectory:     ''
+        }))
+        .on('error', function(err) {
+          console.error(err.message)
+          done()
+          process.exit(1)
+        })
+        .on('end', function() {
+          done()
+          process.exit()
+        })
+    })
+})
+
+gulp.task('setup travis', function() {
+  reports = [ 'lcovonly' ]
+})
+
+gulp.task('travis', [ 'setup travis', 'test' ])
+
+gulp.task('test-watch', function() {
+  gulp.watch(src.concat(testFiles), [ 'test' ])
+})

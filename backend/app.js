5acb79548453379d996a18a6d46e98b829562354
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-03-09 09:25:15 +0100

    Disable es6.forOf transformer for backend
    node and iojs fully support es6 for of loops

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 
-require('babel/register')({ 'blacklist': [ 'react', 'regenerator' ] })
+require('babel/register')({ 'blacklist': [
+  'react'
+, 'es6.forOf'
+, 'regenerator'
+] })
+
 require('./src/main')

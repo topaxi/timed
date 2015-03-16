b8026fc80aa79e8a5008848e4df12dca82b8aa25
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-03-16 17:03:47 +0100

    Allow starting activities without a task

--- frontend/app/components/track-bar.js
+++ frontend/app/components/track-bar.js
@@ -1,4 +1,5 @@
-import Ember from 'ember';
+import Ember    from 'ember'
+import safehtml from 'timed/utils/safehtml'
 
 export default Ember.Component.extend({
   elementId:  'track-bar'
@@ -6,6 +7,15 @@ export default Ember.Component.extend({
 
 , activity: Ember.computed.alias('user.currentActivity')
 , task:     Ember.computed.alias('activity.task')
+, project:  null
+
+, selectedTask: null
+
+, init(...args) {
+    this._super(...args)
+
+    this.store = this.container.lookup('store:main')
+  }
 
 , showCommentField: function() {
     let activity = this.get('activity')
@@ -21,17 +31,39 @@ export default Ember.Component.extend({
     return isTracking || hasTask
   }.property('activity.to', 'task')
 
+, projects: function() {
+    return this.store.find('project')
+  }.property()
+
+, projectOption: function({ data: project }) {
+    return safehtml`<div class="option">
+      <div>${project.get('name')}</div>
+      <div>${project.get('customer.name')}</div>
+    </div>`
+  }
+
 , actions: {
     startNewActivity() {
       this.get('user').then(user =>
         user.startActivity().save()
       )
     }
+  , setProject(value) {
+      this.set('project', value)
+    }
   , setActivityComment(value) {
       let activity = this.get('activity')
 
       activity.set('comment', value)
       activity.save()
+    }
+  , selectTask(value) {
+      // Destructing ember-selectize while calling an action
+      // throws errors, delay setting task a bit
+      Ember.run.next(() => {
+        this.set('project', null)
+        this.set('task',    value)
+      })
     }
   }
 })

c1e9174ca0a67ac81fd898ef01df8dc2c8c67bf0
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-03-17 17:31:46 +0100

    Minor code style fixes

--- frontend/app/components/track-attendance.js
+++ frontend/app/components/track-attendance.js
@@ -3,9 +3,11 @@ import moment from 'moment';
 
 export default Ember.Component.extend({
   tagName: 'span'
+
 , title: function() {
     return this.get('isTracking') ? 'Stop tracking attendance' : 'Track attendance'
   }.property('isTracking')
+
 , label: function() {
     let attendance = this.get('session.user.currentAttendance')
 
@@ -14,12 +16,12 @@ export default Ember.Component.extend({
     }
 
     let from      = attendance.get('from')
-      , to        = attendance.get('to')
-      , today     = moment().startOf('day')
-      , fromToday = moment(from).startOf('day').diff(today)
-      , tformat   = 'hh:mm'
-      , ntformat  = 'YYYY-MM-DD hh:mm'
-      , label     = from.format(fromToday ? ntformat : tformat)
+    let to        = attendance.get('to')
+    let today     = moment().startOf('day')
+    let fromToday = moment(from).startOf('day').diff(today)
+    let tformat   = 'hh:mm'
+    let ntformat  = 'YYYY-MM-DD hh:mm'
+    let label     = from.format(fromToday ? ntformat : tformat)
 
     if (to && to.isValid()) {
       let toToday = moment(to).startOf('day').diff(today)
@@ -33,6 +35,7 @@ export default Ember.Component.extend({
 
     return label
   }.property('session.user.currentAttendance.from', 'session.user.currentAttendance.to', 'refreshLabel')
+
 , fixTooltip: function() {
     let tooltip = this.$('.tip')
 
@@ -43,12 +46,14 @@ export default Ember.Component.extend({
       tooltip.tooltip('show')
     }
   }.observes('title')
+
 , isTracking: function() {
     let attendance = this.get('session.user.currentAttendance')
-      , to         = attendance && attendance.get('to')
+    let to         = attendance && attendance.get('to')
 
     return to && !to.isValid()
   }.property('session.user.currentAttendance.to')
+
 , actions: {
     track: function() {
       this.get('session.user').then(user => {

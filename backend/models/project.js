f1965450d1cb331be05561816844840efad3d978
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-03-19 17:08:34 +0100

    Make project name unique to customer

--- backend/models/project.js
+++ backend/models/project.js
@@ -1,13 +1,19 @@
 import mongoose, { Schema } from 'mongoose'
 
-export default mongoose.model('Project', {
-  'name':        { type: String, required: true, index: { unique: true } }
-, 'customer':    { type: Schema.Types.ObjectId, ref: 'Customer' }
-, 'tracker':     {
+let ProjectSchema = new Schema
+
+ProjectSchema.add({
+  'name':     { type: String, required: true }
+, 'customer': { type: Schema.Types.ObjectId, ref: 'Customer' }
+, 'tracker':  {
     'type': { type: String, enum: [ 'github', 'redmine' ] }
   , 'data': {}
   }
-, 'from':        Date
-, 'to':          Date
-, 'done':        Boolean
+, 'from':     Date
+, 'to':       Date
+, 'done':     Boolean
 })
+
+ProjectSchema.index({ 'name': 1, 'customer': 1 }, { unique: true })
+
+export default mongoose.model('Project', ProjectSchema)

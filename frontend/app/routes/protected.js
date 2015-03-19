95300237a10ae6d0b8815e19c3fcf2bf10227736
Damian Senn <damian.senn@gmail.com>
2015-03-19 07:07:30 +0100

    Preload some data in protected route
    - customers
    - projects
    - tasks
    - user attendances

--- frontend/app/routes/protected.js
+++ frontend/app/routes/protected.js
@@ -1,4 +1,19 @@
-import Ember from 'ember';
-import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import Ember from 'ember'
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin'
 
-export default Ember.Route.extend(AuthenticatedRouteMixin)
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model() {
+    return Ember.RSVP.all([
+      this.store.find('customer')
+    , this.store.find('project')
+    , this.store.find('task')
+    ])
+  }
+
+, afterModel() {
+    let user        = this.get('session.user.id')
+    let attendances = this.store.find('attendance', { user })
+
+    return attendances
+  }
+})

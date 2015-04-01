b5298f6b9e6f57add68d5a23b2a75df7c7fa9a7f
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-04-01 13:42:18 +0200

    Only fetch attendances over one month in inital payload

--- backend/routes/api/v1/init.js
+++ backend/routes/api/v1/init.js
@@ -1,4 +1,6 @@
 import { Router }          from 'express'
+import moment              from 'moment'
+
 import { async }           from '../../../src/async-route'
 import { BadRequestError } from '../../../src/error'
 import * as model          from '../../../models'
@@ -12,10 +14,15 @@ router.use(auth)
 router.get('/payload', async(function*(req, res) {
   if (!req.user || !req.user.id) throw new BadRequestError
 
+  let query = {
+    user: req.user.id
+  , from: { '$gt': moment().startOf('day').subtract(1, 'month') }
+  }
+
   res.send({
     customers:   yield model.Customer.find().exec()
   , projects:    yield model.Project.find().exec()
   , tasks:       yield model.Task.find().exec()
-  , attendances: yield model.Attendance.find({ user: req.user.id }).exec()
+  , attendances: yield model.Attendance.find(query).exec()
   })
 }))

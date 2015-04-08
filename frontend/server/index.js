5f5157c1d6ea2abcb5ea9057cacf516b3edaca21
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-04-08 14:32:47 +0200

    No need to use morgan, ember-cli already logs requests

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -12,10 +12,6 @@ module.exports = function(app) {
   var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
   var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
-  // Log proxy requests
-  var morgan  = require('morgan');
-  app.use(morgan('dev'));
-
   mocks.forEach(function(route) { route(app); });
   proxies.forEach(function(route) { route(app); });
 

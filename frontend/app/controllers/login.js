004ed7cc3b8b98d5342bd7ddb8c279ade1c71806
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-04-13 12:34:19 +0200

    Disable login form while logging in

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -3,4 +3,11 @@ import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
   authenticator: 'authenticator:custom'
+, actions: {
+    authenticate() {
+      this.set('loading', true)
+
+      this._super().finally(() => this.set('loading', false))
+    }
+  }
 })

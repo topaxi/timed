2165e780a2b01ddbce33dc81b869043aa5674ea5
Damian Senn <damian.senn@gmail.com>
2015-04-23 07:44:28 +0200

    Tab left <-> right transitions

--- frontend/app/transitions.js
+++ frontend/app/transitions.js
@@ -1,5 +1,26 @@
 export default function() {
   this.transition(
+    this.fromRoute('project.edit.edit')
+  , this.toRoute('task.index')
+  , this.use('toLeft')
+  , this.reverse('toRight')
+  )
+
+  this.transition(
+    this.fromRoute('team.edit.edit')
+  , this.toRoute('team.edit.manage')
+  , this.use('toLeft')
+  , this.reverse('toRight')
+  )
+
+  this.transition(
+    this.fromRoute('user.edit.edit')
+  , this.toRoute('user.edit.worktime')
+  , this.use('toLeft')
+  , this.reverse('toRight')
+  )
+
+  this.transition(
     this.use('crossFade', { duration: 150 })
   )
 }

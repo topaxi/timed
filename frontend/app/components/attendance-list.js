142514953f4e5fd9482408812c87d6a5e8007c0c
Stefan Heinemann <stefan.heinemann@adfinis-sygroup.ch>
2015-04-28 17:12:59 +0200

    Added activity duration below the activity list
    Also left the attendance duration code (which is unused) for future
    usage

--- frontend/app/components/attendance-list.js
+++ frontend/app/components/attendance-list.js
@@ -1,4 +1,5 @@
 import Ember from 'ember'
+import moment from 'moment'
 
 export default Ember.Component.extend({
   day: null
@@ -13,6 +14,12 @@ export default Ember.Component.extend({
     return user.getAttendancesByDay(this.get('day'))
                .sort((a, b) => b.get('from').diff(a.get('from')))
   }.property('day', 'user.attendances.@each')
+
+, totalDuration: function() {
+    return moment.duration(this.get('attendances').reduce((duration, attendance) => {
+      return duration + attendance.get('to') - attendance.get('from')
+    }, 0)).format('h[h] mm[m]')
+  }.property('attendances.@each.from', 'attendances.@each.to')
 
 , actions: {
     openModal(...args) {

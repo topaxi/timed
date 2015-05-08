3c497c1c20ba665927c55d9d663b5f4582545c08
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-05-08 17:47:43 +0200

    Less blocking, more filtering

--- frontend/app/controllers/project/index.js
+++ frontend/app/controllers/project/index.js
@@ -1,31 +1,39 @@
 import Ember                from 'ember'
 import PagedControllerMixin from 'timed/mixins/paged-controller-mixin'
 
-const { computed, observer } = Ember
+const { computed, run } = Ember
 
 export default Ember.Controller.extend(PagedControllerMixin, {
   queryParams: [ { 'customerId': 'customer' } ]
 , modelSort: [ 'customer.name:asc', 'name:asc' ]
 
+, filter: ''
 , projectFilter: ''
 
 , sortable: computed.alias('projects')
-, projects: computed('model', 'projectFilter', {
+, projects: computed('model', 'filter', {
     get() {
-      let filter   = this.get('projectFilter').toLowerCase()
-      let projects = this.get('model')
+      let filter = this.get('filter').toLowerCase()
 
-      if (filter.length > 2) {
-        projects = projects.filter(project =>
-          project.get('name').toLowerCase().includes(filter)
-        )
-      }
-
-      return projects
+      return this.get('model').filter(project =>
+        project.get('name').toLowerCase().includes(filter)
+      )
     }
   })
 
-, resetPaging: observer('projectFilter', function() {
+, filterProjects() {
     this.send('resetPaging')
-  })
+
+    let filter = this.get('projectFilter')
+
+    if (!filter || filter.length > 2) {
+      this.set('filter', this.get('projectFilter'))
+    }
+  }
+
+, actions: {
+    updateFilter() {
+      run.debounce(this, this.filterProjects, 200)
+    }
+  }
 })

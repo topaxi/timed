a785bd03a97509da02724adad6fd98737599f10d
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-05-08 11:18:57 +0200

    Populate teams in import-devdata.js

--- backend/bin/import-devdata.js
+++ backend/bin/import-devdata.js
@@ -2,19 +2,20 @@ import * as timed from '../models'
 
 ;(async function main() {
   try {
-    let customers = require('../../docker/mongodb/json/customers/data.json')
-
-    await timed.Customer.create(customers)
+    await timed.Customer.create(getCustomers())
     console.log('Customers created')
 
-    for (let customer of customers) {
+    for (let customer of getCustomers()) {
       let projects = getProjects(customer)
       await timed.Project.create(projects)
       console.log(`${projects.length} projects for customer "${customer.name}" created`)
     }
 
-    await timed.User.create(require('../../docker/mongodb/json/users/data.json'))
+    await timed.User.create(getUsers())
     console.log('Users created')
+
+    await timed.Team.create(getTeams())
+    console.log('Teams created')
   }
   catch (e) {
     console.error(e)
@@ -23,6 +24,24 @@ import * as timed from '../models'
     process.exit() // eslint-disable-line
   }
 })()
+
+function getCustomers() {
+  return require('../../docker/mongodb/json/customers/data.json')
+}
+
+function getUsers() {
+  return require('../../docker/mongodb/json/users/data.json')
+}
+
+function getTeams() {
+  let teams = require('../../docker/mongodb/json/teams/data.json')
+
+  for (let team of teams) {
+    team.users = getRandomSubarray(getUsers(), getRandomInt(3, 20))
+  }
+
+  return teams
+}
 
 function getProjects(customer) {
   let projects = require('../../docker/mongodb/json/projects/data.json')

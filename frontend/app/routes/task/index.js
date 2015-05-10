b99c7a5d6465eee0b47bc26730423ade257a0e5a
Damian Senn <damian.senn@gmail.com>
2015-05-10 15:07:01 +0200

    Fix updating project tasks in task index

--- frontend/app/routes/task/index.js
+++ frontend/app/routes/task/index.js
@@ -10,6 +10,6 @@ export default Ember.Route.extend({
 , model() {
     let project = this.modelFor('project.edit')
 
-    return this.store.find('task', { 'project': project.id }, { project })
+    return project.get('tasks')
   }
 })

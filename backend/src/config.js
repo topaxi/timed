d3a58f494401788600fd3e4a43cdaa027a23d25d
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-05-11 10:00:03 +0200

    Allow tests to be run in docker containers

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,14 +2,16 @@ import merge from 'lodash/object/merge'
 
 const defaults = {
   title:      'Timed'
-, mongodb:    'mongodb://127.0.0.1/timed'
+, mongodb:    { host: '127.0.0.1', db: 'timed' }
 , port:       3000
 , trustProxy: 'loopback'
 }
 
 const config = merge({}, defaults, require('../config.json'), {
   port:    process.env.PORT
-, mongodb: process.env.MONGODB
+, mongodb: { host: process.env.MONGO_HOST, db: process.env.MONGO_DB }
 })
+
+config.mongodb = `mongodb://${config.mongodb.host}/${config.mongodb.db}`
 
 export default Object.freeze(config)
--- backend/src/error.js
+++ backend/src/error.js
@@ -1,25 +1,27 @@
 export class HttpError extends Error {
   constructor(message = 'Request Error', status = 500) {
-    super(message)
+    let err = super(message)
 
-    this.status = status
+    this.stack   = err.stack
+    this.message = message
+    this.status  = status
   }
 }
 
 export class NotFoundError extends HttpError {
   constructor(message = 'Not Found') {
-    return super(message, 404)
+    super(message, 404)
   }
 }
 
 export class NotAuthorizedError extends HttpError {
   constructor(message = 'Not Authorized') {
-    return super(message, 401)
+    super(message, 401)
   }
 }
 
 export class BadRequestError extends HttpError {
   constructor(message = 'Bad Request') {
-    return super(message, 400)
+    super(message, 400)
   }
 }

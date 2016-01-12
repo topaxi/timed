4d9139a523340ac42ac1f5f3f0b5b1ad5a5e25d6
Damian Senn <damian.senn@adfinis-sygroup.ch>
2016-01-12 13:00:59 +0100

    Fix backend tests

--- backend/models/project.js
+++ backend/models/project.js
@@ -3,16 +3,16 @@ import mongoose, { Schema } from 'mongoose'
 let ProjectSchema = new Schema
 
 ProjectSchema.add({
-  'name':           { type: String, required: true }
-, 'customer':       { type: Schema.Types.ObjectId, ref: 'Customer' }
-, 'projectLeaders': [ { type: Schema.Types.ObjectId, ref: 'User' } ]
-, 'tracker':  {
-    'type': { type: String, enum: [ 'github', 'redmine' ] }
-  , 'data': {}
-  }
-, 'from':     Date
-, 'to':       Date
-, 'done':     Boolean
+  'name':           { type: String, required: true },
+  'customer':       { type: Schema.Types.ObjectId, ref: 'Customer' },
+  'projectLeaders': [ { type: Schema.Types.ObjectId, ref: 'User' } ],
+  'tracker':  {
+    'type': { type: String, enum: [ 'github', 'redmine' ] },
+    'data': {}
+  },
+  'from':  Date,
+  'to':    Date,
+  'done':  Boolean
 })
 
 ProjectSchema.index({ 'name': 1, 'customer': 1 }, { unique: true })
